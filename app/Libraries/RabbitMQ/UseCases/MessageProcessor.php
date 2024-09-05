1454dcc7347fe0ef1d09993e8168d6023a936060
igorlamoia_hbi <igor@hbifinanceira.com>
2024-09-05 13:59:28 -0300

    feat: using model

--- app/Libraries/RabbitMQ/UseCases/MessageProcessor.php
+++ app/Libraries/RabbitMQ/UseCases/MessageProcessor.php
@@ -2,15 +2,18 @@
 
 namespace App\Commands\UseCases;
 use PhpAmqpLib\Message\AMQPMessage;
+use UserTransferModel;
 
 class MessageProcessor
 {
   public static function execute(AMQPMessage  $msg) {
     // CLI::write('Received: ' . $msg->body, 'green');
     // echo json_decode($msg);
-    $db = db_connect();
     $data = json_decode($msg->getBody(), true);
-    $db->table('user_transfer')->insert($data);
+    // $db = db_connect();
+    // $db->table('user_transfer')->insert($data);
+    $userTransfer = new UserTransferModel();
+    $userTransfer->insert($data);
     $msg->ack();
     // Here you can add your logic to process the message
   }
---
+++ app/Models/UserTransferModel.php
@@ -0,0 +1,30 @@
+<?php
+
+use CodeIgniter\Model;
+
+class UserTransferModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'user_transfer';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        "name",
+        "value"
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+}
